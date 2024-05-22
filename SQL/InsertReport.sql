31a18c5a600a4bedf7ab2e178d977e2dcacf6536
김찬별 <47071366+cksquf98@users.noreply.github.com>
2024-05-22 20:57:57 +0900

    Add files via upload

---
+++ SQL/InsertReport.sql
@@ -0,0 +1,11 @@
+insert into reports(report_idx,reporting_user,reported_user,report_category,report_date) values(1,2,1,1,'2024-05-22 20:49:00');
+insert into reports(report_idx,reporting_user,reported_user,report_category,report_date) values(2,3,1,1,'2024-05-22 20:51:32');
+insert into reports(report_idx,reporting_user,reported_user,report_category,report_date) values(3,4,1,1,'2024-05-22 19:58:12');
+insert into reports(report_idx,reporting_user,reported_user,report_category,report_date) values(4,5,1,1,'2024-05-22 21:01:54');
+
+insert into reports(report_idx,reporting_user,reported_user,report_category,report_date) values(5,9,7,2,'2024-05-22 13:41:24');
+insert into reports(report_idx,reporting_user,reported_user,report_category,report_date) values(6,1,7,2,'2024-05-22 13:55:30');
+
+insert into reports(report_idx,reporting_user,reported_user,report_category,report_date) values(7,6,10,3,'2024-05-22 09:17:24');
+
+
